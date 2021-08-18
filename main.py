2eb95389f0cda91cdd30814dd5713c011b75db3f
Jonathan Rochelli <jonathan.rochelli@gmail.com>
2021-08-18 14:39:53 +0200

    Add route to collect all the leagues

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def getCountries():
     )
     return response
 
-# Route to collect the leagues
+# Route to collect the leagues filtered by country
 @app.route("/leagues")
 def getLeagues():
     country = request.args.get('country')
@@ -38,11 +38,36 @@ def getLeagues():
     )
     return response
 
+# Route to collect all the leagues 
+@app.route("/leagues/all")
+def getAllLeagues():
+    data = leagues.find({ "$or" : [{"country.name" : "France"}, {"country.name" : "Germany"}, {"country.name" : "Italy"}, {"country.name" : "Portugal"}, {"country.name" : "Spain"}, {"country.name" : "England"}, {"country.name" : "Belgium"}, {"country.name" : "World"}] })
+    response = {
+        "France" : [],
+        "Italy" : [],
+        "Portugal" : [],
+        "Germany" : [],
+        "Belgium" : [],
+        "England" : [],
+        "World" : [],
+        "Spain" : []
+    }
+    for league in data:
+        response[league["country"]["name"]].append(league["league"])
+    response = app.response_class(
+        response=json.dumps(response),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
 # Route to collect the fixtures
 @app.route("/fixtures")
 def getFixtures():
+    league = request.args.get('league')
     response = app.response_class(
-        response=json_util.dumps(list(fixtures.find())[0]),
+        response=json_util.dumps(fixtures.find({"league.name" : league})),
         status=200,
         mimetype='application/json'
     )
